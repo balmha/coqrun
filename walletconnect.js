f91fc4447dd6acb4908f81f1db57b40b7856463d
balmha <balmacedanicolas4@gmail.com>
2024-01-02 14:36:01 -0300

    walletconnect module

---
+++ walletconnect.js
@@ -0,0 +1,11 @@
+/* To connect using MetaMask */
+async function connect() {
+    if (window.ethereum) {
+    
+     await window.ethereum.request({ method: "eth_requestAccounts" });
+     window.web3 = new Web3(window.ethereum);
+     
+    } else {
+     console.log("No wallet");
+    }
+   }
