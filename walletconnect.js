966eace7069daaaed29540df8251cd0a413279e9
balmha <balmacedanicolas4@gmail.com>
2024-01-02 14:57:57 -0300

    test wallet connect 1

--- walletconnect.js
+++ walletconnect.js
@@ -1,11 +1,16 @@
+var Web3 = require('web3');
+var provider = 'https://avalanche-mainnet.infura.io';
+var web3Provider = new Web3.providers.HttpProvider(provider);
+var web3 = new Web3(web3Provider);
+/* web3.eth.getBlockNumber().then((result) => {
+  console.log("Latest Ethereum Block is ",result);
+}); */
+
 /* To connect using MetaMask */
 async function connect() {
-    if (window.ethereum) {
-    
-     await window.ethereum.request({ method: "eth_requestAccounts" });
-     window.web3 = new Web3(window.ethereum);
-     
+    if (web3.eth) {
+        web3.eth.requestAccounts().then(console.log);
     } else {
      console.log("No wallet");
     }
-   }
+}
